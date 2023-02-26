1462f14f29dd5960a5583f01972fef19d2427f84
sebcontreras <seb.contreras24@gmail.com>
2023-02-25 19:22:47 -0700

    added IDs, added margins

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,13 +1,13 @@
 import { Card, CardHeader, Heading, CardBody, Text } from '@chakra-ui/react'
 
-export default function Question(props: {title: string, text: string}){
+export default function Question(props: {id: string, title: string, text: string}){
     return (
         <Card textAlign='center'>
             <CardHeader>
                 <Heading>{props.title}</Heading>
             </CardHeader>
             <CardBody textAlign='left'>
-                <Text fontSize={['sm', 'md']}>
+                <Text fontSize={['md', 'lg']}>
                     {props.text}
                 </Text>
             </CardBody>
