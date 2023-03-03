401e29b03f0091cbc6bbb2ccac8d05a99f7d14f1
Jhappy77 <joel.happ@shaw.ca>
2023-03-03 00:59:10 -0700

    Update url to deployed backend

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,4 @@
-export const API_URL = `http://localhost:3000`;
-export const SOCKET_URL = `http://localhost:8008`;
+// export const API_URL = `http://localhost:3000`;
+export const API_URL = `https://cpsc-559-project.vercel.app`;
+export const SOCKET_URL = `https://cpsc-559-project.vercel.app:8008`;
+// export const SOCKET_URL = `http://localhost:8008`;
