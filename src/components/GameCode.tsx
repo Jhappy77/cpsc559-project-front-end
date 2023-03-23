45efdbef04f982e75a0bae4f5dc53ecd40fe2c0f
Desiree Leal <desiree.leal1@ucalgary.ca>
2023-03-23 15:52:42 -0600

    changed  box to card to work with dark/light mode

--- src/components/GameCode.tsx
+++ src/components/GameCode.tsx
@@ -1,16 +1,11 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Card, CardHeader, Heading, CardBody, Text } from "@chakra-ui/react";
 
 export default function GameCode(props: { id: string | undefined; }) {
   return (
-   <Box p={2} maxW='lg' maxH='lg' bg='white' display='flex' alignItems='baseline' borderRadius='md'>
-        <Box >
-        <Text fontFamily={`'Comfortaa', sans-serif`} fontSize='l' color="black">
-            Game Code:
-        </Text>
-        </Box>
-        <Box color='black' fontWeight='bold' letterSpacing='wide' fontSize='xl'>
-             <p>&nbsp;{props.id}</p>
-        </Box>
-    </Box>
+    <Card size='sm'>
+        <CardBody textAlign='center' fontSize='lg'>
+            <Text>Game Code: <b>{props.id}</b></Text>
+        </CardBody>
+    </Card>
   );
 }
