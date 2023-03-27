71c7e84df2878e242c652f262ce4e1ea70a40b22
Vic (Vu) Phan <65319838+vicphan@users.noreply.github.com>
2023-03-27 17:51:05 -0600

    add names of players who joined on host's screen

--- src/hooks/useGetGamePlayers.ts
+++ src/hooks/useGetGamePlayers.ts
@@ -1,31 +1,43 @@
 import { useAppDispatch, useAppSelector } from "../state/reduxHooks";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { API_URL } from "../settings";
-import { setQuestion } from "../state/questionSlice";
-import { setGotQuestion } from "../state/gameSlice";
+import { setPlayers } from "../state/gameSlice";
 
-// TBD
-// export function useGetQuestion() {
-//   const { gameCode, gameCreationCallTs } = useAppSelector(state => state.game);
-//   const dispatch = useAppDispatch();
-//   useEffect(() => {
-//     if (!gameCode) return;
-//     if (gameCreationCallTs === undefined) return; 
-//     axios
-//       .get(`${API_URL}/games/question/${gameCode}`)
-//       .then(response => {
-//         console.log(response);
-//         const questionResponse = response.data;
-//         if (questionResponse) {
-//           dispatch(setQuestion(questionResponse));
-//           dispatch(setGotQuestion(true));
-//           console.log("Got question!");
-//         } else throw new Error("No question data recieved");
-//       })
-//       .catch(reason => {
-//         console.error("Unhandled error in useGetQuestion");
-//         console.error(reason);
-//       });
-//   }, [gameCreationCallTs, dispatch]);
-// }
+export function useGetGamePlayers() {
+  const { gameCode, gameCreationCallTs, gameStarted } = useAppSelector(state => state.game);
+  const dispatch = useAppDispatch();
+  const [ intervalID, setIntervalID ] = useState<NodeJS.Timeout | undefined>(undefined);
+
+  useEffect(() => {
+    if (gameCode !== undefined){
+        // keep getting players from backend every second 
+        const interval = setInterval(() => {
+            if (!gameCode) return;
+            if (gameCreationCallTs === undefined) return;
+            axios
+              .get(`${API_URL}/games/players/${gameCode}`)
+              .then(response => {
+                const playerArr = new Array(response.data.length);
+                for (let i=0; i < response.data.length; i++){
+                    playerArr[i] = response.data[i].name;
+                }
+                dispatch(setPlayers(playerArr));
+              })
+              .catch(reason => {
+                console.error("Unhandled error in useGetGamePlayers");
+                console.error(reason);
+              });
+        }, 1000);
+        setIntervalID(interval);
+        return () => clearInterval(interval);
+    }
+  }, [gameCode]);
+
+  useEffect(() => {
+    // stop getting players from backend once game has started
+    if (gameStarted) {
+        clearInterval(intervalID);
+    }
+  }, [gameStarted])
+}
