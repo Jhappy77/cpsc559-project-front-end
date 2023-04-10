9b63bf106a24e214f77dd9cfbf9d543e1e342e31
sebcontreras <seb.contreras24@gmail.com>
2023-04-10 12:12:04 -0600

    added condition on  create player

--- src/hooks/useCreatePlayer.ts
+++ src/hooks/useCreatePlayer.ts
@@ -2,28 +2,34 @@ import { useAppDispatch, useAppSelector } from "../state/reduxHooks";
 import axios from "axios";
 import { useEffect } from "react";
 import { API_URL } from "../settings";
-import { setHasJoinedGame } from "../state/gameSlice"
+import { setHasJoinedGame } from "../state/gameSlice";
 
 export function useCreatePlayer() {
-    const { name } = useAppSelector(state => state.player);
-    const { gameCode } = useAppSelector(state => state.game);
-    const dispatch = useAppDispatch();
-    useEffect(() => {
-        if (!name || !gameCode || name === "" || gameCode === "") return;
-        axios
-            .post(`${API_URL}/players`,
-                {
-                    "name": name,
-                    "joinCode": gameCode
-                })
-            .then(response => {
-                response.status === 201 ? dispatch(setHasJoinedGame(true)) : dispatch(setHasJoinedGame(false));
-                console.log(`API Call to: /players, response: ${response.status}`);
-            })
-            .catch(reason => {
-                console.error("Unhandled error in useCreatePlayer");
-                console.error(reason);
-                alert("A user with this name already exists, please try again with a new name!");
-            });
-    }, [name]);
+  const { name } = useAppSelector(state => state.player);
+  const { gameCode } = useAppSelector(state => state.game);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (!name || !gameCode || name === "" || gameCode === "") return;
+    axios
+      .post(`${API_URL}/players`, {
+        name: name,
+        joinCode: gameCode,
+      })
+      .then(response => {
+        if (response.status === 201) {
+          dispatch(setHasJoinedGame(true));
+        } else if (response.status === 203) {
+          dispatch(setHasJoinedGame(false));
+          alert("This game has already begun, please try again with a different game.");
+        } else {
+          dispatch(setHasJoinedGame(false));
+        }
+        console.log(`API Call to: /players, response: ${response.status}`);
+      })
+      .catch(reason => {
+        console.error("Unhandled error in useCreatePlayer");
+        console.error(reason);
+        alert("A user with this name already exists, please try again with a new name!");
+      });
+  }, [name]);
 }
